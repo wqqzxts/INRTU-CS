ea87078b961229e34b7a8126dcd38836ffbb8c14
Timur Semennikov <wqqzxts@gmail.com>
2025-04-22 21:33:05 +0800

    made form markup for string-gui

---
+++ gui-lab2/string-gui/string-gui/Form1.cs
@@ -0,0 +1,11 @@
+namespace string_gui {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
