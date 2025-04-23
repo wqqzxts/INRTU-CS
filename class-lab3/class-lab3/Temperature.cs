a442260606e650ba87437ac20ef1436771eefe54
Timur Semennikov <wqqzxts@gmail.com>
2025-04-23 08:53:48 +0800

    made convertion function and tests

--- class-lab3/class-lab3/Temperature.cs
+++ class-lab3/class-lab3/Temperature.cs
@@ -1,5 +1,5 @@
 ﻿namespace class_lab3 {
-    public enum Type { Ra, K, C };
+    public enum Type { Ra, K, C, Fa };
     public class Temperature {
         private double value;
         private Type type;
@@ -9,8 +9,9 @@
             this.type = type;
         }
 
-        public static Temperature operator +(Temperature instance, double value) {                     
-            return new Temperature(instance.value + value, instance.type); ;
+        public static Temperature operator +(Temperature instance, double value) {
+            return new Temperature(instance.value + value, instance.type);
+            ;
         }
 
         public static Temperature operator +(double value, Temperature instance) {
@@ -44,8 +45,50 @@
             return instance / value;
         }
 
+        public Temperature To(Type newType) {
+            if (this.type == newType) {
+                return new Temperature(this.value, newType);
+            }
+
+            double kelvinValue = 0;
+
+            switch (this.type) {
+                case Type.Ra:
+                    kelvinValue = this.value * 5 / 9;
+                    break;
+                case Type.K:
+                    kelvinValue = this.value;
+                    break;
+                case Type.C:
+                    kelvinValue = this.value + 273.15;
+                    break;
+                case Type.Fa:
+                    kelvinValue = (this.value + 459.67) * 5 / 9;
+                    break;
+            }
+
+            double newValue = 0;
+            switch (newType) {
+                case Type.Ra:
+                    newValue = kelvinValue * 9 / 5;
+                    break;
+                case Type.K:
+                    newValue = kelvinValue;
+                    break;
+                case Type.C:
+                    newValue = kelvinValue - 273.15;
+                    break;
+                case Type.Fa:
+                    newValue = kelvinValue * 9 / 5 - 459.67;
+                    break;
+            }
+
+            return new Temperature(newValue, newType);
+        }
+
         public string Verbose() {
             string typeFormatted = "";
+            double roundedValue = Math.Round(this.value, 2);
 
             switch (this.type) {
                 case Type.Ra:
@@ -57,9 +100,12 @@
                 case Type.C:
                     typeFormatted = "по Цельсию";
                     break;
+                case Type.Fa:
+                    typeFormatted = "по Фаренгейту";
+                    break;
             }
 
-            return String.Format("{0} {1}", this.value, typeFormatted);
+            return String.Format("{0} {1}", roundedValue, typeFormatted);
         }
     }
 }
--- class-lab3/class-lab3Tests2/TemperatureTests.cs
+++ class-lab3/class-lab3Tests2/TemperatureTests.cs
@@ -19,6 +19,9 @@ namespace class_lab3.Tests {
 
             temperature = new Temperature(671.67, Type.Ra);
             Assert.AreEqual("671.67 по Ранкину", temperature.Verbose());
+
+            temperature = new Temperature(72, Type.Fa);
+            Assert.AreEqual("72 по Фаренгейту", temperature.Verbose());
         }
 
         [TestMethod()]
@@ -44,9 +47,107 @@ namespace class_lab3.Tests {
 
         [TestMethod()]
         public void DivideValueTest() {
-            var temperature = new Temperature(12.4, Type.K);
+            var temperature = new Temperature(12.4, Type.Fa);
             temperature /= 2;
-            Assert.AreEqual("6.2 по Кельвину", temperature.Verbose());
+            Assert.AreEqual("6.2 по Фаренгейту", temperature.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_SameType_ReturnsSameValue() {
+            var temperature = new Temperature(100, Type.C);
+            var result = temperature.To(Type.C);            
+            Assert.AreEqual("100 по Цельсию", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_CelsiusToFahrenheit() {
+            var temperature = new Temperature(0, Type.C);
+            var result = temperature.To(Type.Fa);
+            Assert.AreEqual("32 по Фаренгейту", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_FahrenheitToCelsius() {
+            var temperature = new Temperature(32, Type.Fa);
+            var result = temperature.To(Type.C);
+            Assert.AreEqual("0 по Цельсию", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_CelsiusToKelvin() {
+            var temperature = new Temperature(0, Type.C);
+            var result = temperature.To(Type.K);
+            Assert.AreEqual("273.15 по Кельвину", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_KelvinToCelsius() {
+            var temperature = new Temperature(273.15, Type.K);
+            var result = temperature.To(Type.C);
+            Assert.AreEqual("0 по Цельсию", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_RankineToKelvin() {
+            var temperature = new Temperature(491.67, Type.Ra);
+            var result = temperature.To(Type.K);
+            Assert.AreEqual("273.15 по Кельвину", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_KelvinToRankine() {
+            var temperature = new Temperature(273.15, Type.K);
+            var result = temperature.To(Type.Ra);
+            Assert.AreEqual("491.67 по Ранкину", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_FahrenheitToRankine() {
+            var temperature = new Temperature(32, Type.Fa);
+            var result = temperature.To(Type.Ra);
+            Assert.AreEqual("491.67 по Ранкину", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_RankineToFahrenheit() {
+            var temperature = new Temperature(491.67, Type.Ra);
+            var result = temperature.To(Type.Fa);
+            Assert.AreEqual("32 по Фаренгейту", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_AbsoluteZeroKelvinToCelsius() {
+            var temperature = new Temperature(0, Type.K);
+            var result = temperature.To(Type.C);
+            Assert.AreEqual("-273.15 по Цельсию", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_AbsoluteZeroKelvinToFahrenheit() {
+            var temperature = new Temperature(0, Type.K);
+            var result = temperature.To(Type.Fa);
+            Assert.AreEqual("-459.67 по Фаренгейту", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_AbsoluteZeroKelvinToRankine() {
+            var temperature = new Temperature(0, Type.K);
+            var result = temperature.To(Type.Ra);
+            Assert.AreEqual("0 по Ранкину", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_BoilingWaterCelsiusToFahrenheit() {
+            var temperature = new Temperature(100, Type.C);
+            var result = temperature.To(Type.Fa);
+            Assert.AreEqual("212 по Фаренгейту", result.Verbose());
+        }
+
+        [TestMethod()]
+        public void To_FreezingWaterFahrenheitToRankine() {
+            var temperature = new Temperature(32, Type.Fa);
+            var result = temperature.To(Type.Ra);
+            Assert.AreEqual("491.67 по Ранкину", result.Verbose());
         }
     }
 }
