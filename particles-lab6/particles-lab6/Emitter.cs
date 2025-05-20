a0cc04fb87cc2fd03936fce4ad73674d2c75127f
Timur Semennikov <wqqzxts@gmail.com>
2025-05-20 11:33:55 +0800

    refactored to use emitter class as a particle renderning and state updating handler

---
+++ particles-lab6/particles-lab6/Emitter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace particles_lab6 {
+    public class Emitter {
+        public List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public void updateState(PictureBox picDisplay) {
+            for (var i = 0; i < 10; ++i) {
+                if (particles.Count < 500) {
+                    var particle = new Particle();
+                    particle.X = Particle.rand.Next(picDisplay.Width);
+                    particle.Y = Particle.rand.Next(picDisplay.Height);
+                    particles.Add(particle);
+                } else {
+                    break;
+                }
+            }
+
+            foreach (var particle in particles) {
+                particle.life -= 1;
+                if (particle.life < 0) {
+                    particle.life = 20 + Particle.rand.Next(100);
+                    particle.X = Particle.rand.Next(picDisplay.Width);
+                    particle.Y = Particle.rand.Next(picDisplay.Height);
+
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.speedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.speedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                    particle.radius = 2 + Particle.rand.Next(10);
+                } else {
+                    particle.X += particle.speedX;
+                    particle.Y += particle.speedY;
+                }
+            }
+        }
+
+        public void render(Graphics g) {
+            foreach (var particle in particles) {
+                particle.Draw(g);
+            }
+        }
+    }
+}
--- particles-lab6/particles-lab6/Form1.cs
+++ particles-lab6/particles-lab6/Form1.cs
@@ -1,5 +1,6 @@
 namespace particles_lab6 {
     public partial class Form1 : Form {
+        Emitter emitter = new Emitter();
         private List<Particle> particles = new List<Particle>();
         private int counter = 0;
         public Form1() {
@@ -10,54 +11,14 @@ namespace particles_lab6 {
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            updateState();
+            emitter.updateState(picDisplay);
 
             using (var g = Graphics.FromImage(picDisplay.Image)) {
                 g.Clear(Color.Black);
-                render(g);
+                emitter.render(g);
             }
 
             picDisplay.Invalidate();
-        }
-
-        private void updateState() {
-            for (var i = 0; i < 10; ++i) {
-                if (particles.Count < 500)
-                {
-                    var particle = new Particle();
-                    particle.X = Particle.rand.Next(picDisplay.Width);
-                    particle.Y = Particle.rand.Next(picDisplay.Height);
-                    particles.Add(particle);
-                } else {
-                    break;
-                }
-            }
-
-            foreach (var particle in particles) {
-                particle.life -= 1;
-                if (particle.life < 0) {
-                    particle.life = 20 + Particle.rand.Next(100);
-                    particle.X = Particle.rand.Next(picDisplay.Width);
-                    particle.Y = Particle.rand.Next(picDisplay.Height);
-
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.speedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.speedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-
-                    particle.radius = 2 + Particle.rand.Next(10);
-                } else {
-                    particle.X += particle.speedX;
-                    particle.Y += particle.speedY;
-                }
-            }
-        }
-
-        private void render(Graphics g) {
-            foreach (var particle in particles) {
-                particle.Draw(g);
-            }
         }
     }
 }
